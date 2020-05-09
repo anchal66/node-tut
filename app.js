c025bea5a96d6812ee6fccff3b90704d4b0586b0
Avinash Anchal <anchalavinash66@gmail.com>
2020-05-10 00:55:21 +0530

    Added nodemon as global for live server

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 console.log(msg);
 
-console.log(validator.isEmail('anchal@gg.com'));
-
-console.log(validator.isURL('https://www.google.com'))
+console.log(chalk.green('Success!'));
