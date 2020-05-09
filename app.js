1e193c1fec6ffd270070a98474f57e1c35bf581f
Avinash Anchal <anchalavinash66@gmail.com>
2020-05-10 00:04:43 +0530

    demobtarte validator package from npm

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const validator = require('validator')
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
-
 console.log(msg);
+
+console.log(validator.isEmail('anchal@gg.com'));
+
+console.log(validator.isURL('https://www.google.com'))
